0bc7ec5d78c7810f4946a4fa0bd2ac1892cf356c
Roth <andres.mesa1707@gmail.com>
2023-07-24 18:57:54 -0400

    Primer Commit

---
+++ KrosfyNewsHub/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KrosfyNewsHub.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SearchResult()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult SinglePost()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
