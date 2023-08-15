36bd9b1577e61480b0f428186269ecaa70020002
Alex Romero <alex.romero@datalog.it>
2023-08-15 12:21:18 +0200

    WebService

---
+++ KrosfyNewsHub/Service.asmx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace KrosfyNewsHub
+{
+    /// <summary>
+    /// Descrizione di riepilogo per Service
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Per consentire la chiamata di questo servizio Web dallo script utilizzando ASP.NET AJAX, rimuovere il commento dalla riga seguente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Service : WebService
+    {
+        // Meteremos a disposicion api's que permiten enviar las ultimas noticias del dia
+        // Una api que se encarga de gestionar o mandar email a los usuarios subscritos
+        // Un api que registra un usuario en el portal - para darle acceso al administrador
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
