440d333fd2a8fcb898824ca27bc1d4deb014e8e5
Alex Romero <alex.romero@datalog.it>
2023-08-15 11:50:13 +0200

    login

---
+++ KrosfyNewsHub/App_Start/IdentityConfig.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+
+[assembly: OwinStartup(typeof(KrosfyNewsHub.App_Start.IdentityConfig))]
+
+namespace KrosfyNewsHub.App_Start
+{
+    public class IdentityConfig
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                CookieName = string.IsNullOrEmpty(ConfigurationManager.AppSettings.Get("COOKIES")) ? "KrosfyNews" : ConfigurationManager.AppSettings.Get("COOKIES"),
+                LoginPath = new PathString("/Login/Logout"),
+            });
+        }
+    }
+}
